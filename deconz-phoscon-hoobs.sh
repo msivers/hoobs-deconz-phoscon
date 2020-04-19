e232b72fa4781804e5569cff167eaf6c9e011796
Bobby Slope <info@hoobs.org>
2020-04-19 22:04:44 +0200

    Update deconz-phoscon-hoobs.sh

--- deconz-phoscon-hoobs.sh
+++ deconz-phoscon-hoobs.sh
@@ -32,6 +32,12 @@ sudo sh -c "echo 'deb http://phoscon.de/apt/deconz \
             $(lsb_release -cs)-beta main' > \
             /etc/apt/sources.list.d/deconz.list"
 sudo apt update
+sudo apt install i2c-tools build-essential raspberrypi-kernel-headers
+curl -y -O -L -k https://github.com/dresden-elektronik/raspbee2-rtc/archive/master.zip
+unzip master.zip
+cd raspbee2-rtc-master
+make
+sudo make install
 sudo apt install deconz
 echo "DeConz & Phoscon installed."
 echo "----------------------------------------------------------------"
