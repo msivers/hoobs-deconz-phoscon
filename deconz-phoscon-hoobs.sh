11a8932033b0cc682a20ab811017b7c347172c4c
Bobby Slope <info@hoobs.org>
2020-04-19 20:46:01 +0200

    Update deconz-phoscon-hoobs.sh

--- deconz-phoscon-hoobs.sh
+++ deconz-phoscon-hoobs.sh
@@ -1,47 +1,85 @@
 #!/bin/bash
 
 echo "----------------------------------------------------------------"
-echo "This will install Node-RED for HOOBS 3"
+echo "This will install DeConz and Phoscon for HOOBS 3"
 echo "----------------------------------------------------------------"
 echo " "
 echo " "
-echo "Installing Node-RED core....."
-mkdir /home/hoobs/.node-red
-cd /home/hoobs/.node-red
-sudo npm install -g --silent --unsafe-perm --force node-red
-echo "Node-RED core installed."
+echo "Enable Raspberry Serial Port.."
+sudo raspi-config nonint do_serial 1
+echo "Raspberry Serial Port enabled"
 echo "----------------------------------------------------------------"
-echo "Installing Raspberry-Pi Nodes....."
-sudo npm install -g --silent --unsafe-perm --save node-red-contrib-homebridge-automation node-red-node-pi-gpio node-red-node-random node-red-node-ping node-red-contrib-play-audio node-red-node-smooth node-red-node-serialport
-echo "Raspberry-Pi Nodes installed"
+echo "Get Phoscon Public Key....."
+wget -O - http://phoscon.de/apt/deconz.pub.key | \
+sudo apt-key add -
+echo "Installing....."
+sudo sh -c "echo 'deb http://phoscon.de/apt/deconz \
+            $(lsb_release -cs) main' > \
+            /etc/apt/sources.list.d/deconz.list"
+sudo apt update
+sudo apt install deconz
+echo "DeConz & Phoscon installed."
 echo "----------------------------------------------------------------"
-echo "Setup Auto Start Service....."
-cat > /etc/systemd/system/nodered_boot.service <<EOL
-# systemd service file to start Node-RED
-[Unit]
-Description=Node-RED graphical event wiring tool
-Wants=network.target
-Documentation=http://nodered.org/docs/hardware/raspberrypi.html
-[Service]
-Type=simple
-User=root
-WorkingDirectory=/home/hoobs
-Nice=5
-Environment="NODE_OPTIONS=--max_old_space_size=256"
-ExecStart=/usr/local/bin/node-red-pi $NODE_OPTIONS $NODE_RED_OPTIONS
-KillSignal=SIGINT
-Restart=on-failure
-SyslogIdentifier=Node-RED
-[Install]
-WantedBy=multi-user.target
-EOL
-cd /usr/local/bin
-sudo ln -s /home/hoobs/.node-red/node_modules/.bin/node-red-pi node-red-pi
-sudo systemctl daemon-reload
-sudo systemctl enable nodered_boot.service
-sudo systemctl restart nodered_boot.service
-echo "Node-RED successfully installed"
+echo "Installing Wiring Pi (for Rpi4B)....."
+cd /tmp
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
+echo "Wiring Pi (for Rpi4B) installed"
 echo "----------------------------------------------------------------"
-echo "Node-RED Interface is reachable at hoobs.local:1880"
-echo "HOOBS    Interface is reachable at hoobs.local"
+echo "Updating DeConz"
+wget https://www.dresden-elektronik.de/rpi/deconz/deconz-latest.deb
+sudo dpkg -i deconz-latest.deb
+echo "DeConz updated"
 echo "----------------------------------------------------------------"
+echo "Disable GUI for DeConz...."
+sudo systemctl disable deconz-gui.service
+echo "GUI for DeConz disabled"
+
+echo "----------------------------------------------------------------"
+echo "You need to reboot your Device now"
+echo "----------------------------------------------------------------"
+echo "Phoscon Interface is reachable at hoobs.local"
+echo "HOOBS   Interface is reachable at hoobs.local:8080"
+echo "----------------------------------------------------------------"
+confirm() {
+#
+# syntax: confirm [<prompt>]
+#
+# Prompts the user to enter Yes or No and returns 0/1.
+#
+# This  program  is free software: you can redistribute it and/or modify  it
+# under the terms of the GNU General Public License as published by the Free
+# Software  Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This  program  is  distributed  in the hope that it will  be  useful,  but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public  License
+# for more details.
+#
+# You  should  have received a copy of the GNU General Public License  along
+# with this program. If not, see <http://www.gnu.org/licenses/>
+#
+#  04 Jul 17   0.1   - Initial version - MEJT
+#
+  local _prompt _default _response
+ 
+  if [ "$1" ]; then _prompt="$1"; else _prompt="Would you like to reboot?"; fi
+  _prompt="$_prompt [y/n] ?"
+ 
+  # Loop forever until the user enters a valid response (Y/N or Yes/No).
+  while true; do
+    read -r -p "$_prompt " _response
+    case "$_response" in
+      [Yy][Ee][Ss]|[Yy]) # Yes or Y (case-insensitive).
+        return sudo reboot
+        ;;
+      [Nn][Oo]|[Nn])  # No or N.
+        return sudo reboot
+        ;;
+      *) # Anything else (including a blank) is invalid.
+        ;;
+    esac
+  done
+}
+
