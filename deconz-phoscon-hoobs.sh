738bc88ff13a22182717143356853131e7d52d0f
Bobby Slope <info@hoobs.org>
2020-04-19 20:57:42 +0200

    Update deconz-phoscon-hoobs.sh

--- deconz-phoscon-hoobs.sh
+++ deconz-phoscon-hoobs.sh
@@ -3,44 +3,52 @@
 echo "----------------------------------------------------------------"
 echo "This will install DeConz and Phoscon for HOOBS 3"
 echo "----------------------------------------------------------------"
-echo " "
-echo " "
-echo "Enable Raspberry Serial Port.."
-sudo raspi-config nonint do_serial 1
-echo "Raspberry Serial Port enabled"
-echo "----------------------------------------------------------------"
-echo "Get Phoscon Public Key....."
-wget -O - http://phoscon.de/apt/deconz.pub.key | \
-sudo apt-key add -
-echo "Installing....."
-sudo sh -c "echo 'deb http://phoscon.de/apt/deconz \
-            $(lsb_release -cs) main' > \
-            /etc/apt/sources.list.d/deconz.list"
-sudo apt update
-sudo apt install deconz
-echo "DeConz & Phoscon installed."
-echo "----------------------------------------------------------------"
-echo "Installing Wiring Pi (for Rpi4B)....."
-cd /tmp
-wget https://project-downloads.drogon.net/wiringpi-latest.deb
-sudo dpkg -i wiringpi-latest.deb
-echo "Wiring Pi (for Rpi4B) installed"
-echo "----------------------------------------------------------------"
-echo "Updating DeConz"
-wget https://www.dresden-elektronik.de/rpi/deconz/deconz-latest.deb
-sudo dpkg -i deconz-latest.deb
-echo "DeConz updated"
-echo "----------------------------------------------------------------"
-echo "Disable GUI for DeConz...."
-sudo systemctl disable deconz-gui.service
-sudo systemctl disable nginx.service
-echo "GUI for DeConz disabled"
 
 echo "----------------------------------------------------------------"
-echo "You need to reboot your Device now"
+echo "After the installation is completed the Device performs an"
+echo "reboot and you can access Phoscon and HOOBS as following:"
 echo "----------------------------------------------------------------"
 echo "Phoscon Interface is reachable at hoobs.local"
 echo "HOOBS   Interface is reachable at hoobs.local:8080"
 echo "----------------------------------------------------------------"
-
+read -p "Would you like to instaLL now? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+echo "Enable Raspberry Serial Port.."
+yes y | sudo raspi-config nonint do_serial 1
+echo "Raspberry Serial Port enabled"
+echo "----------------------------------------------------------------"
+echo "Get Phoscon Public Key....."
+yes y | wget -O - http://phoscon.de/apt/deconz.pub.key | \
+yes y | sudo apt-key add -
+echo "Installing....."
+yes y | sudo sh -c "echo 'deb http://phoscon.de/apt/deconz \
+            $(lsb_release -cs) main' > \
+            /etc/apt/sources.list.d/deconz.list"
+yes y | sudo apt update
+yes y | sudo apt install deconz
+echo "DeConz & Phoscon installed."
+echo "----------------------------------------------------------------"
+echo "Installing Wiring Pi (for Rpi4B)....."
+yes y | cd /tmp
+yes y | wget https://project-downloads.drogon.net/wiringpi-latest.deb
+yes y | sudo dpkg -i wiringpi-latest.deb
+echo "Wiring Pi (for Rpi4B) installed"
+echo "----------------------------------------------------------------"
+echo "Updating DeConz"
+yes y | wget https://www.dresden-elektronik.de/rpi/deconz/deconz-latest.deb
+sudo dpkg -i deconz-latest.deb
+echo "DeConz updated"
+echo "----------------------------------------------------------------"
+echo "Disable GUI for DeConz...."
+yes y | sudo systemctl disable deconz-gui.service
+yes y | sudo systemctl disable nginx.service
+echo "GUI for DeConz disabled"
+echo "----------------------------------------------------------------"
+echo "Rebooting now....."
+echo "----------------------------------------------------------------"
+wait 10
+sudo reboot
+fi
 
