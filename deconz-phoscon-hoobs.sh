5b22504123030bcf2217c032d41aa791ad4839d3
Bobby Slope <info@hoobs.org>
2020-04-19 20:49:32 +0200

    Update deconz-phoscon-hoobs.sh

--- deconz-phoscon-hoobs.sh
+++ deconz-phoscon-hoobs.sh
@@ -33,6 +33,7 @@ echo "DeConz updated"
 echo "----------------------------------------------------------------"
 echo "Disable GUI for DeConz...."
 sudo systemctl disable deconz-gui.service
+sudo systemctl disable nginx.service
 echo "GUI for DeConz disabled"
 
 echo "----------------------------------------------------------------"
@@ -41,45 +42,5 @@ echo "----------------------------------------------------------------"
 echo "Phoscon Interface is reachable at hoobs.local"
 echo "HOOBS   Interface is reachable at hoobs.local:8080"
 echo "----------------------------------------------------------------"
-confirm() {
-#
-# syntax: confirm [<prompt>]
-#
-# Prompts the user to enter Yes or No and returns 0/1.
-#
-# This  program  is free software: you can redistribute it and/or modify  it
-# under the terms of the GNU General Public License as published by the Free
-# Software  Foundation, either version 3 of the License, or (at your option)
-# any later version.
-#
-# This  program  is  distributed  in the hope that it will  be  useful,  but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public  License
-# for more details.
-#
-# You  should  have received a copy of the GNU General Public License  along
-# with this program. If not, see <http://www.gnu.org/licenses/>
-#
-#  04 Jul 17   0.1   - Initial version - MEJT
-#
-  local _prompt _default _response
- 
-  if [ "$1" ]; then _prompt="$1"; else _prompt="Would you like to reboot?"; fi
-  _prompt="$_prompt [y/n] ?"
- 
-  # Loop forever until the user enters a valid response (Y/N or Yes/No).
-  while true; do
-    read -r -p "$_prompt " _response
-    case "$_response" in
-      [Yy][Ee][Ss]|[Yy]) # Yes or Y (case-insensitive).
-        return sudo reboot
-        ;;
-      [Nn][Oo]|[Nn])  # No or N.
-        return sudo reboot
-        ;;
-      *) # Anything else (including a blank) is invalid.
-        ;;
-    esac
-  done
-}
+
 
