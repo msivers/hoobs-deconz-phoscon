5c4931d6752ac957643818dfff0e6615ddabc933
Bobby Slope <info@hoobs.org>
2020-04-20 07:52:40 +0200

    Update deconz-phoscon-hoobs.sh

--- deconz-phoscon-hoobs.sh
+++ deconz-phoscon-hoobs.sh
@@ -1,5 +1,5 @@
 ##################################################################################################
-# hoobs-core                                                                                     #
+# hoobs-deconz-phoscon                                                                           #
 # Copyright (C) 2020 HOOBS                                                                       #
 #                                                                                                #
 # This program is free software: you can redistribute it and/or modify                           #
